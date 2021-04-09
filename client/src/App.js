a3516da9ff4098346eb1af1e11038ef0bf2ec751
Ryland Quirk <quirk2580@gmail.com>
2021-04-09 16:18:58 -0500

    re-add app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from "./components/Nav";
+import Jumbo from "./components/Jumbotron";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <>
+        <Nav />
+        <Jumbo />
+        <Switch>
+          <Route exact path="/">
+            <Search />
+          </Route>
+          <Route path="/savedbooks">
+            <Saved />
+          </Route>
+        </Switch>
+      </>
+    </Router>
   );
 }
 
